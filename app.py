246180f6973170c7373fbb131f338a86b6ebc411
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-05-02 22:03:43 +0530

    Fetching data using mongoDB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from flask import Flask , render_template ,request
 from pymongo import MongoClient
+
 """
 import os
 db_user = os.environ['USER']
@@ -6,18 +8,33 @@ db_pass = os.environ['PASS']
 print(db_user, db_pass)
 """
 
-name=input("Enter Your Name  : ")
+app=Flask("myapp")
 
-client = MongoClient('mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+@app.route("/")
+def home():
+    return render_template("login.html")
 
-result = client['mark_jan']['marks'].find({"name" : name , "marks" : 75} )
-for i in result:
-    mk=int(i['marks'])
-    print("Marks obtained by " + name + " are : " + str(mk))
-    if mk >= 90:
-        print("Congrats You Got First Division")
-    else:
-        print("Oops ! You need to World Hard") 
+@app.route("/result")
+def result():
+    if request.method == "GET":
+        name=request.args.get("name")
+        roll=request.args.get("roll")
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        result = client['flask_db']['marks'].find({"name" : name , "rollno" : int(roll)} )
+        if result.count() == 0:
+            return "Wrong Credentials"
 
+        for i in result:
+            mk=int(i['marks'])
+            output="Marks obtained by " + name + " are : " + str(mk)
+            if mk >= 90:
+               print("Congrats You Got First Division")
+            else:
+                print("Oops ! You need to World Hard")
+            return str(output)
+                      
+        
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
