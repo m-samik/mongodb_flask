9c4c784ef6246c99a6d6bf346e10fda63bc7f8e4
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2021-05-02 15:43:56 +0530

    CLI Working

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from pymongo import MongoClient
+"""
+import os
+db_user = os.environ['USER']
+db_pass = os.environ['PASS']
+print(db_user, db_pass)
+"""
 
 name=input("Enter Your Name  : ")
 
 client = MongoClient('mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
-filter={}
-result = client['mark_jan']['marks'].find({"name" : name})
+
+result = client['mark_jan']['marks'].find({"name" : name , "marks" : 75} )
 for i in result:
-    print(i)
-#test
+    mk=int(i['marks'])
+    print("Marks obtained by " + name + " are : " + str(mk))
+    if mk >= 90:
+        print("Congrats You Got First Division")
+    else:
+        print("Oops ! You need to World Hard") 
+
 
 
